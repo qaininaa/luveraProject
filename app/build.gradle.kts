0dc9e0d5815cce87b5b2d4c500d0c0286116717d
qaininaa <karinaghsn@gmail.com>
2025-04-23 10:31:23 +0700

    home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
